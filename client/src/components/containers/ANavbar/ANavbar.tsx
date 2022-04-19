ae31ac5a35ffdcdf3ac6b6519141d08634839036
xreider <xreider@yandex.ru>
2022-04-19 12:39:56 +1000

    first commit

--- client/src/components/containers/ANavbar/ANavbar.tsx
+++ client/src/components/containers/ANavbar/ANavbar.tsx
@@ -1,4 +1,4 @@
-import { FC, MutableRefObject, useEffect, useRef, useState } from "react";
+import { FC, useState } from "react";
 import cn from "classnames";
 import { useTranslation } from "react-i18next";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
@@ -10,13 +10,12 @@ import ANavItem from "./ANavItem";
 import ASearchForm from "./ASearchForm";
 import { useStoreANavbar } from "stores/useStoreANavbar";
 
-import useScrollDirection, {
-  EScrollDir,
-} from "hooks/common/useScrollDirection";
-import { useGetCssValueNum } from "hooks/common/useGetCssVars";
-import { ECssSpeedValue } from "constants/CssValues/ECssSpeedValue";
-
 export interface PANavbar {}
+
+export enum EScrollDir {
+  up = "up",
+  down = "down",
+}
 
 const ANavbar: FC<PANavbar> = () => {
   const { t } = useTranslation();
@@ -24,70 +23,21 @@ const ANavbar: FC<PANavbar> = () => {
     // propsANavbar: { onDesktopTopStyle },
     propsANavbar: { onDesktopTopClassName },
   } = useStoreANavbar();
-  const scrollDir = useScrollDirection();
-  const [toTopNum, setToTopNum] = useState(0);
-  const [zeroToTop, setZeroToTop] = useState(true);
+  // const scrollDir = useScrollDirection();
+  const [currPos, setCurrPos] = useState(0);
+  const [dir, setDir] = useState(EScrollDir.up);
   const [positionAbsolute, setPositionAbsolute] = useState(true);
-  const [animationUp, setAnimationUp] = useState(false);
-  const [normalSpeed] = useGetCssValueNum([ECssSpeedValue.slowest]);
-  let timeoutId: MutableRefObject<NodeJS.Timeout | null> = useRef(null);
   useScrollPosition(
     ({ prevPos, currPos }) => {
-      console.log(currPos.y);
-      setToTopNum(Math.abs(currPos.y));
-      setZeroToTop(Math.abs(currPos.y) === 0);
+      setDir(currPos.y - prevPos.y < 0 ? EScrollDir.down : EScrollDir.up);
+      setCurrPos(Math.abs(currPos.y));
+      setPositionAbsolute(currPos.y === 0);
     },
     [],
     undefined,
     false,
     300
   );
-
-  useEffect(() => {
-    console.log("scrollDir", scrollDir);
-    console.log("toTopNum", toTopNum);
-    console.log("zeroToTop", zeroToTop);
-
-    if (zeroToTop) {
-      console.log("!!!!!!!!!!!!!!!!!", 1);
-      setPositionAbsolute(true);
-      if (timeoutId.current) {
-        clearTimeout(timeoutId.current);
-      }
-      // } else if (toTopNum < 110 && scrollDir !== EScrollDir.up) {
-      //   // console.log("!!!!!!!!!!!!!!!!!", 2);
-      //   // setPositionAbsolute(true);
-      // } else if (zeroToTop && scrollDir === EScrollDir.down && toTopNum < 110) {
-      //   // console.log("!!!!!!!!!!!!!!!!!", 3);
-      //   // setPositionAbsolute(true);
-      // } else if (toTopNum < 130 && scrollDir === EScrollDir.down) {
-      //   // console.log("!!!!!!!!!!!!!!!!!", 4);
-      //   // setPositionAbsolute(true);
-    } else if (scrollDir === EScrollDir.down) {
-      console.log("!!!!!!!!!!!!!!!!!", 5);
-      // setAnimationUp(true);
-      setPositionAbsolute(true);
-      // timeoutId.current = setTimeout(() => {
-      // }, normalSpeed);
-    } else if (toTopNum > 110 && scrollDir === EScrollDir.up) {
-      console.log("!!!!!!!!!!!!!!!!!", 6);
-      setPositionAbsolute(false);
-      // if (timeoutId.current) {
-      //   clearTimeout(timeoutId.current);
-      // }
-    }
-  }, [scrollDir, toTopNum, zeroToTop, setPositionAbsolute, normalSpeed]);
-
-  // useEffect(() => {
-  //   if (scrollDir === EScrollDir.down) {
-  //     console.log("!!!!!!!!!!!!!!!!!", 5);
-  //     setAnimationUp(true);
-  //     // setPositionAbsolute(true);
-  //     setTimeout(() => {
-  //       setAnimationUp(false);
-  //     }, normalSpeed);
-  //   }
-  // }, [scrollDir, normalSpeed]);
 
   return (
     <div
@@ -97,9 +47,10 @@ const ANavbar: FC<PANavbar> = () => {
         onDesktopTopClassName,
         onDesktopTopClassName && st["transparentBG"],
         positionAbsolute && st["positionAbsolute"],
-        (toTopNum > 110 || !positionAbsolute) && st[`positionFixed`]
-        // scrollDir === EScrollDir.down && st[`scrollDirDown`],
-        // animationUp && st["animationUp"]
+        ((!positionAbsolute && dir === EScrollDir.up) || currPos >= 90) &&
+          st["fixed"],
+        dir === EScrollDir.down && currPos >= 90 && st["hide"]
+        // currPos === 0 && st["closeToTop"]
       )}
     >
       <div className={cn(st.ANavbar_panel)}>
